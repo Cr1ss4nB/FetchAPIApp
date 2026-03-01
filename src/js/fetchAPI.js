b4bf3057fb802a375f2a037227474fb503c4daa5
Andrés Basto <cabalacode@gmail.com>
2026-02-28 23:51:14 -0500

    feat: Punto 1 y 2 del taller listos.

---
+++ src/js/fetchAPI.js
@@ -0,0 +1,79 @@
+const tabla = document.getElementById("tablaUsuarios");
+const detalle = document.getElementById("detalleUsuario");
+
+function cargarUsuarios() {
+
+    fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Error en la petición GET: " + response.status);
+            }
+            return response.json();
+        })
+        .then(usuarios => {
+            console.log("Usuarios obtenidos correctamente:", usuarios);
+            usuarios.slice(0, 30).forEach(usuario => {
+                tabla.innerHTML += `
+                    <tr>
+                        <td>
+                            <a href="#" onclick="verDetalle(${usuario.id})">
+                                ${usuario.name}
+                            </a>
+                        </td>
+                        <td>${usuario.email}</td>
+                        <td>${usuario.phone}</td>
+                        <td>${usuario.company.name}</td>
+                        <td>${usuario.website}</td>
+                    </tr>
+                `;
+            });
+        })
+        .catch(error => {
+            console.error("Error capturado en GET:", error);
+        });
+} cargarUsuarios();
+
+function verDetalle(id) {
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Error en detalle usuario: " + response.status);
+            }
+            return response.json();
+        })
+        .then(usuario => {
+            console.log("Detalle del usuario seleccionado:", usuario);
+            detalle.innerHTML = `
+                <p>ID: ${usuario.id}</p>
+
+                <p>Nombre:
+                    <input id="nombre" value="${usuario.name}">
+                </p>
+
+                <p>Username:
+                    <input id="username" value="${usuario.username}">
+                </p>
+
+                <p>Email:
+                    <input id="email" value="${usuario.email}">
+                </p>
+
+                <p>Teléfono:
+                    <input id="phone" value="${usuario.phone}">
+                </p>
+
+                <p>Website:
+                    <input id="website" value="${usuario.website}">
+                </p>
+
+                <p>Ciudad: ${usuario.address.city}</p>
+                <p>Calle: ${usuario.address.street}</p>
+                <p>Suite: ${usuario.address.suite}</p>
+                <p>Compañía: ${usuario.company.name}</p>
+                <p>CatchPhrase: ${usuario.company.catchPhrase}</p>
+            `;
+        })
+        .catch(error => {
+            console.error("Error capturado en detalle:", error);
+        });
+}
